3b7a4f99d185724924eb205676edc19089fd8634
Diego Valdovinos Carbajal <diegovaldovinoscarbajal@hotmail.com>
2025-01-23 20:02:50 -0600

    Proyecto plantas

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  // definicion de imagenes
+  img01: string = "/img/planta-01.jpg";
+  img02: string = "/img/planta-02.jpg";
+  img03: string = "/img/planta-03.jpg";
+  img04: string = "/img/planta-04.jpg";
+  img05: string = "/img/planta-05.jpg";
+  img06: string = "/img/planta-06.jpg";
+  img07: string = "/img/planta-07.jpg";
+  img08: string = "/img/planta-08.jpg";
+  img09: string = "/img/planta-09.jpg";
+  img10: string = "/img/planta-10.jpg";
+  img11: string = "/img/planta-11.jpg";
+  img12: string = "/img/planta-12.jpg";
+  // definicion de nombres de plantas
+  plant01: string = "Bromelia";
+  plant02: string = "Medinilla";
+  plant03: string = "Acedera";
+  plant04: string = "Cereus Forbessil";
+  plant05: string = "Dracena";
+  plant06: string = "Livistona";
+  plant07: string = "Malva";
+  plant08: string = "Nogal";
+  plant09: string = "Petunia";
+  plant10: string = "Regaliz";
+  plant11: string = "Ricino";
+  plant12: string = "Saúco";
+  // definicion de precios
+  price01: number = 68.0;
+  price02: number = 68.0;
+  price03: number = 68.0;
+  price04: number = 68.0;
+  price05: number = 68.0;
+  price06: number = 68.0;
+  price07: number = 68.0;
+  price08: number = 68.0;
+  price09: number = 68.0;
+  price10: number = 68.0;
+  price11: number = 68.0;
+  price12: number = 68.0;
+}
